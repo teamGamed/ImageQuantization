a1b97f799a52c0d4f9839b9790c509d60747a4d7
mahmoodsherif <ma13121999@gmail.com>
2019-12-04 01:44:16 +0200

    add class Data for store the static data between function

---
+++ ImageQuantization/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageQuantization
+{
+    class Data
+    {
+        // have the distinct colors   
+        public static List<RGBPixel> colors;
+        // the graph
+        public static Dictionary<RGBPixel , Dictionary<RGBPixel , double>> distances;
+        // MST
+        public static Dictionary< RGBPixel ,List<RGBPixel>> MST;
+        // the components after Extract the clusters
+        public static List<List<RGBPixel>> comps;
+        // the mapping color
+        public static Dictionary<RGBPixel, RGBPixel> colorMap;
+    }
+}
