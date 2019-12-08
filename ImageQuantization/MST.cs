3fdd0147449cd7f1b2322c86f91ec838d742ecb1
Bohoty <omarbohoty@gmail.com>
2019-12-08 19:06:11 +0000

    Change from 1-based to 0-based

--- ImageQuantization/MST.cs
+++ ImageQuantization/MST.cs
@@ -7,29 +7,21 @@ namespace ImageQuantization
 {
     class MST
     {
-        // getting MST with total complexity Elog(V^2) = 2 Elog(V) 
-        
-        Dictionary<RGBPixel, RGBPixel> p;
+        static private Dictionary<int ,int> p = new Dictionary<int, int>();
 
-        private Dictionary<RGBPixel, int> rnk;
-        // check if the 2 nodes are equal
-        bool equals(RGBPixel first, RGBPixel second)
+        static private Dictionary<int, int> rnk = new Dictionary<int, int>();
+        
+        int parent(int node)
         {
-            if (first.red == second.red && first.blue == second.blue && first.green == second.green)
-                return true;
-            return false;
-        }
-        RGBPixel parent(RGBPixel node)
-        {
-            if (equals(node, p[node]))
+            if (node == p[node])
                 return node;
             return p[node] = parent(p[node]);
         }
         // connect 2 nodes
-        void connect(RGBPixel first, RGBPixel second)
+        void connect(int first, int second)
         {
-            RGBPixel a = parent(first), b = parent(second);
-            if (equals(a, b)) return;
+            int a = parent(first), b = parent(second);
+            if (a == b) return;
             if (rnk[a] < rnk[b])
                 p[a] = b;
             else if (rnk[b] < rnk[a])
@@ -41,35 +33,35 @@ namespace ImageQuantization
             }
         }
         // check if the 2 nodes are already in the same component
-        bool isConnected(RGBPixel first, RGBPixel second)
+        bool isConnected(int first, int second)
         {
-            if (equals(parent(first), parent(second)))
+            if (parent(first) == parent(second))
                 return true;
             return false;
         }
-        void getMST(int v)
+        void getMST()
         {
-            for (int i = 1; i <= v; i++)
+            int v = Data.colorsNum;
+            for (int cur = 0; cur < v; cur++)
             {
-                RGBPixel cur = Data.colors[i];
                 p[cur] = cur;
                 rnk[cur] = 0;
             }
-                List<Tuple<double, RGBPixel, RGBPixel>> Edge = new List<Tuple<double, RGBPixel, RGBPixel>>();
-            for (int i = 1; i <= v; i++)
+            List < Tuple < double, int, int > > Edge = new List<Tuple<double, int, int>>();
+            for (int i = 0; i < v; i++)
             {
-                for (int j = 1; j <= v; j++)
+                for (int j = 0; j < v; j++)
                 {
-                    Edge.Add(Tuple.Create(Data.distances[i][j], Data.colors[i], Data.colors[j]));
+                    Edge.Add(Tuple.Create(Data.distances[i,j], i, j));
                 }
             }
             Edge.Sort();
-            foreach(Tuple<double, RGBPixel, RGBPixel> edge in Edge)
+            foreach(Tuple<double, int, int> edge in Edge)
             {
                 if (isConnected(edge.Item2, edge.Item3)) continue;
                 connect(edge.Item2, edge.Item3);
-                Data.MSTList[get_id(edge.Item2)].Add(get_id(edge.Item3));
-                Data.MSTList[get_id(edge.Item3)].Add(get_id(edge.Item3));
+                Data.MSTList[edge.Item2].Add(edge.Item3);
+                Data.MSTList[edge.Item3].Add(edge.Item3);
             }
         }
     }
