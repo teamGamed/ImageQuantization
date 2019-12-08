7fd896895ce63254500b7daaf8675398b95d8c15
Bohoty <omarbohoty@gmail.com>
2019-12-08 17:37:05 +0000

    get_id is not implemented yet & the code needs to be reviewed.

--- ImageQuantization/MST.cs
+++ ImageQuantization/MST.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms.VisualStyles;
+using Microsoft.SqlServer.Server;
 
 namespace ImageQuantization
 {
     class MST
     {
         // getting MST with total complexity Elog(V^2) = 2 Elog(V) 
-
+        
         Dictionary<RGBPixel, RGBPixel> p;
+
+        private Dictionary<RGBPixel, int> rnk;
         // check if the 2 nodes are equal
         bool equals(RGBPixel first, RGBPixel second)
         {
@@ -23,8 +28,17 @@ namespace ImageQuantization
         // connect 2 nodes
         void connect(RGBPixel first, RGBPixel second)
         {
-            // O(log n) per operation
-            p[parent(first)] = parent(second);
+            RGBPixel a = parent(first), b = parent(second);
+            if (equals(a, b)) return;
+            if (rnk[a] < rnk[b])
+                p[a] = b;
+            else if (rnk[b] < rnk[a])
+                p[b] = a;
+            else
+            {
+                rnk[a]++;
+                p[b] = a;
+            }
         }
         // check if the 2 nodes are already in the same component
         bool isConnected(RGBPixel first, RGBPixel second)
@@ -33,9 +47,30 @@ namespace ImageQuantization
                 return true;
             return false;
         }
-        void getMST()
+        void getMST(int v)
         {
-
+            for (int i = 1; i <= v; i++)
+            {
+                RGBPixel cur = Data.colors[i];
+                p[cur] = cur;
+                rnk[cur] = 0;
+            }
+                List<Tuple<double, RGBPixel, RGBPixel>> Edge = new List<Tuple<double, RGBPixel, RGBPixel>>();
+            for (int i = 1; i <= v; i++)
+            {
+                for (int j = 1; j <= v; j++)
+                {
+                    Edge.Add(Tuple.Create(Data.distances[i][j], Data.colors[i], Data.colors[j]));
+                }
+            }
+            Edge.Sort();
+            foreach(Tuple<double, RGBPixel, RGBPixel> edge in Edge)
+            {
+                if (isConnected(edge.Item2, edge.Item3)) continue;
+                connect(edge.Item2, edge.Item3);
+                Data.MSTList[get_id(edge.Item2)].Add(get_id(edge.Item3));
+                Data.MSTList[get_id(edge.Item3)].Add(get_id(edge.Item3));
+            }
         }
     }
 }
